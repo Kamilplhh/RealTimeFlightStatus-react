9cbb29cd5fcf753294cc8c9608ef5be06608d98b
Kamilplhh <Kamil_PLHH@wp.pl>
2023-07-06 21:56:43 +0200

    Dump data test

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,44 @@
-import { useEffect, useState } from "react"
+import React, { useEffect, useState } from "react"
 
 export default function App() {
- const [data,setData]=useState([]);
-  const getData=()=>{
-    fetch('jsonTestFile/data.json'
-    ,{
-      headers : { 
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-       }
-    }
-    )
-      .then(function(response){
-        console.log(response)
-        return response.json();
-      })
-      .then(function(myJson) {
-        console.log(myJson);
-        setData(myJson)
-      });
-  }
-  useEffect(()=>{
-    getData()
-  },[])
+  const [flights, setFlights] = useState([])
 
+  const getData = () => {
+    fetch('jsonTestFile/data.json')
+      .then(response => {
+        return response.json()
+      })
+      .then(data => {
+        console.log(data)
+        setFlights(data)
+      })
+  }
+  useEffect(() => {
+    getData()
+  }, [])
+
+  const Flight = ({ flightDate, flightStatus, arrival, airlane }) => (
+    <div>
+      <p>{flightDate}</p>
+      <p>{flightStatus}</p>
+      <p>{arrival}</p>
+      <p>{airlane}</p>
+    </div>
+  )
+
+  return (
+    <div>
+      {flights.map(flight => (
+        <div key={flight.id}>
+          <Flight
+            flightDate={flight.flightDate}
+            flightStatus={flight.flightStatus}
+            arrival={flight.arrival.scheduled}
+            airlane={flight.airlane.name}
+          />
+        </div>
+        ))
+      }
+     </div>
+  )
 }
