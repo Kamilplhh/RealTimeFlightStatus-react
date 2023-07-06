87ff0e4bd946e192648475eba7bacf5e22134483
Kamilplhh <Kamil_PLHH@wp.pl>
2023-07-06 18:02:45 +0200

    Testing API json file

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,27 @@
+import { useEffect, useState } from "react"
+
 export default function App() {
-  return "Hello"
+ const [data,setData]=useState([]);
+  const getData=()=>{
+    fetch('jsonTestFile/data.json'
+    ,{
+      headers : { 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+       }
+    }
+    )
+      .then(function(response){
+        console.log(response)
+        return response.json();
+      })
+      .then(function(myJson) {
+        console.log(myJson);
+        setData(myJson)
+      });
+  }
+  useEffect(()=>{
+    getData()
+  },[])
+
 }
