841bb1a7ef4b3250ab8f6dbba9c1d4696c7121cd
Kamilplhh <Kamil_PLHH@wp.pl>
2023-07-12 18:27:27 +0200

    Data form

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
-import React from "react"
+import React, { useState } from "react"
 import useFetch from "./useFetch";
 
 export default function App() {
   const { flights, loading, error } = useFetch("jsonTestFile/data.json");
+  const [NewAirLine, setNewAirLine] = useState("");
+  const [NewFlightNumber, setNewFlightNumber] = useState("");
+  const current = new Date();
+  const today = `${current.getFullYear()}-${('0' + (current.getMonth() + 1)).slice(-2)}-${('0' + current.getDate()).slice(-2)}`;
+  const [newDate, setNewDate] = useState(today);
 
   if (loading) {
     return (
@@ -30,35 +35,53 @@ export default function App() {
     );
   }
 
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    onSubmit(NewAirLine, NewFlightNumber, newDate)
+
+    setNewAirLine("")
+    setNewFlightNumber("")
+    setNewDate(today)
+  }
 
   if (error) console.log(error);
 
   const Flight = ({ id, flightDate, flightStatus, arrival, delay }) => (
-
     <div key={id}>
       <p>{flightDate}</p>
       <p>{flightStatus}</p>
       <p>{arrival}</p>
       <p>{delay}</p>
     </div>
-    
   )
 
   return (
-
-    <div className="dataBlock">
-      {flights.map(flight => (
-        <div className="flightData">
-          <Flight
-            id={flight.id}
-            flightDate={flight.flight_date}
-            flightStatus={flight.flight_status}
-            arrival={flight.arrival.scheduled}
-            delay={flight.departure.delay}
-          />
+    <>
+      <form onSubmit={handleSubmit} className="flightForm">
+        <div className="formRows">
+          <input type="text" id="airLine" value={NewAirLine} autoComplete="off" onChange={e => setNewAirLine(e.target.value)} required />
+          <input type="text" id="flightNumber" value={NewFlightNumber} autoComplete="off" onChange={e => setNewFlightNumber(e.target.value)} required />
+          <input type="date" name="deadline" value={newDate} onChange={e => setNewDate(e.target.value)} id="date" min={today} />
+          <button className="btn">Add</button>
         </div>
-      ))
-      }
-    </div>
+
+      </form>
+
+      <div className="dataBlock">
+        {flights.map(flight => (
+          <div className="flightData">
+            <Flight
+              id={flight.id}
+              flightDate={flight.flight_date}
+              flightStatus={flight.flight_status}
+              arrival={flight.arrival.scheduled}
+              delay={flight.departure.delay}
+            />
+          </div>
+        ))
+        }
+      </div>
+    </>
   )
 }
